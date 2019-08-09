44750c19c2d1a5faf161ec93a1c837b1c71ea1db
aakashDeveloper <ahanda205@gmail.com>
2019-08-09 03:13:38 +0100

    a

---
+++ javascript/second.js
@@ -0,0 +1,87 @@
+var a = 10
+10 == 10
+10 == 10
+true
+10 == "10"
+true
+10 === "10"
+false
+true == 1
+true
+false == 0
+true
+true === 1
+false
+**  = Single Equal is assignment
+**  == Compare value 
+**  === Compare value and data type
+
+null
+undefined
+
+var b = undefined;
+undefined
+typeof(b)
+"undefined"
+
+var a = null;
+undefined
+typeof(a)
+"object"
+null == undefined
+true
+null === undefined
+false
+
+var a = null;
+
+truthy
+<0 , true
+
+falsy
+>=0, null, undefined , false
+
+
+var a !== 10
+var a = 10;
+undefined
+a<10
+false
+a>10
+false
+a==10
+true
+a!=10
+false
+a!=11
+
+true
+
+var a = 10
+undefined
+var b = 20
+undefined
+a == 10 && b ==20
+true
+var b = 21
+undefined
+a == 10 && b ==20
+false
+a == 10 || b ==20
+true
+
+
+
+false && false = false
+false || false =  false
+true && false = false
+true || false = true
+true && true = true
+true || true = true
+
+
+var a  =  10
+var b= "london"
+var c = true
+
+a == 10 && (b == "Delhi" || c == 1)
