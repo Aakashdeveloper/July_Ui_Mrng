95615846a137042cc14b1e3a35d257d2d2af192e
aakashDeveloper <ahanda205@gmail.com>
2019-08-13 03:24:39 +0100

    a

--- javascript/third.js
+++ javascript/third.js
@@ -2,3 +2,96 @@ var a = 10
 var b = 20
 var out = a+b
 console.log(out)
+
+var a = "Captian America"
+
+var a = "Captian America"
+undefined
+a.length
+15
+a.toUpperCase()
+"CAPTIAN AMERICA"
+a.toLowerCase()
+"captian america"
+var a = "     This is Captian America.  "
+undefined
+a.length
+31
+a.trim()
+"This is Captian America."
+var a = "This    is Captian America.  "
+undefined
+a.trim()
+"This    is Captian America."
+a.replace('America','India')
+"This    is Captian India.  "
+a[0]
+"T"
+a[1]
+"h"
+a.charAt(10)
+" "
+var a = "This is Captian America."
+undefined
+a.charAt(10)
+"p"
+a[10]
+"p"
+a[0]
+"T"
+a.split(' ')
+(4) ["This", "is", "Captian", "America."]
+
+var url = "http://localhost:8085/product/mobile/iphone/iphonex"
+var outupt = url.split('/')
+
+var a = "India"
+var out = a.split('')
+
+var a = "India"
+var out = a.split('')
+undefined
+out
+(5) ["I", "n", "d", "i", "a"]
+var a = "London"
+undefined
+a.slice(1)
+"ondon"
+a.slice(2)
+"ndon"
+var a = "London"
+undefined
+a.slice(2,4)
+"nd"
+a.slice(20)
+""
+
+
+var a = " joHN"
+var b = a.trim();
+b.charAt(0).toUpperCase()+b.slice(1).toLowerCase();
+var c = b.charAt(0).toUpperCase()
+var d = b.slice(1).toLowerCase();
+
+var a = "10"
+var b = "10"
+a+b
+1010
+
+var a = "10"
+var b = "10"
+a+b
+"1010"
+parseInt(a)+parseInt(b)
+20
+var c = "12.4435"
+undefined
+parseInt(a)+parseInt(b)+paresInt(c)
+parseInt(a)+parseInt(b)+parseInt(c)
+32
+parseInt(a)+parseInt(b)+parseFloat(c)
+32.4435
+var a = 10
+undefined
+a.toString()
+"10"
